7aac0b421c6711f0b11e4c9d0d5baa5e43d5816e
Bunny_Hacker <bunnyadtsvpap9473@gmail.com>
2024-10-27 09:46:59 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,19 @@
+marks=[]
+s1=int(input("Enter marks :"))
+marks.append(s1)
+s2=int(input("Enter marks:"))
+marks.append(s2)
+s3=int(input("Enter marks:"))
+marks.append(s3)
+s4=int(input("Enter marks"))
+marks.append(s4)
+s5=int(input("Enter marks"))
+marks.append(s5)
+s6=int(input("Enter marks"))
+marks.append(s6)
+s7=int(input("Enter marks"))
+marks.append(s7)
+
+marks.shot()
+
+print(marks)
